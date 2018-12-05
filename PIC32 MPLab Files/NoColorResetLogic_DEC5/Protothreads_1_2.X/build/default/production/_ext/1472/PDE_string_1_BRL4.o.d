8a95fd9691a735ebec9374b3356ec627ffc5e876
Russell Silva <rms438@cornell.edu>
2018-12-05 14:02:51 -0500

    Update

---
+++ PIC32 MPLab Files/NoColorResetLogic_DEC5/Protothreads_1_2.X/build/default/production/_ext/1472/PDE_string_1_BRL4.o.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1472/PDE_string_1_BRL4.o:  \
+ ../PDE_string_1_BRL4.c  \
+../config_1_2_2a.h  \
+../pt_cornell_1_2_2a.h  \
+ ../port_expander_brl4.h  \
+../tft_master.h  \
+../tft_gfx.h 
---
+++ PIC32 MPLab Files/NoColorResetLogic_DEC5/Protothreads_1_2.X/build/default/production/_ext/1472/TFT_ADC_read_SCAN.o.d
@@ -0,0 +1,6 @@
+build/default/production/_ext/1472/TFT_ADC_read_SCAN.o:  \
+ ../TFT_ADC_read_SCAN.c  \
+../config_1_2_2a.h  \
+../pt_cornell_1_2_2a.h  \
+ ../tft_master.h  \
+../tft_gfx.h 
